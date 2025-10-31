a6b23e3fcd646d601f12e8dff43b210566a8918a
bytecreater <nihalahemad2003@gmail.com>
2025-10-31 19:58:12 +0530

    Optimal Approach

--- NeetCode/Arrays and Hashing/Lonngest_consecutive_sequence.java
+++ NeetCode/Arrays and Hashing/Lonngest_consecutive_sequence.java
@@ -1,25 +1,52 @@
+// import java.util.HashSet;
+
+// public class Lonngest_consecutive_sequence {
+//     public static int LongestSequence(int[] nums){
+//         int res = 0;
+//         HashSet<Integer> set = new HashSet<>();
+//         for(int num : nums){
+//             set.add(num);
+//         }
+
+//         for(int num : nums){
+//             int streak = 0 ,  curr = num;
+//             while(set.contains(curr)){
+//                 streak++;
+//                 curr++;
+//             }
+//             res = Math.max(streak,res);
+//         }
+//         return res;
+//     }
+//     public static void main(String[] args) {
+//         int[] nums = {2,20,4,10,3,4,5};
+//         System.out.print("Length of longest conscuitive sequence : " + LongestSequence(nums));
+//     }
+// }
+
+
 import java.util.HashSet;
-
 public class Lonngest_consecutive_sequence {
-    public static int LongestSequence(int[] nums){
-        int res = 0;
-        HashSet<Integer> set = new HashSet<>();
-        for(int num : nums){
-            set.add(num);
+    public static int longestConsecutive(int[] nums) {
+        HashSet<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
         }
+        int longest = 0;
 
-        for(int num : nums){
-            int streak = 0 ,  curr = num;
-            while(set.contains(curr)){
-                streak++;
-                curr++;
+        for (int num : numSet) {
+            if (!numSet.contains(num - 1)) {
+                int length = 1;
+                while (numSet.contains(num + length)) {
+                    length++;
+                }
+                longest = Math.max(longest, length);
             }
-            res = Math.max(streak,res);
         }
-        return res;
+        return longest;
     }
-    public static void main(String[] args) {
+         public static void main(String[] args) {
         int[] nums = {2,20,4,10,3,4,5};
-        System.out.print("Length of longest conscuitive sequence : " + LongestSequence(nums));
+        System.out.print("Length of longest conscuitive sequence : " + longestConsecutive(nums));
     }
 }
