88d01229285514adfa9d6a8ec9246f51d48215ca
bytecreater <nihalahemad2003@gmail.com>
2025-11-05 08:27:00 +0530

    Valid Palindrom II

--- NeetCode/Arrays and Hashing/Valid_Palindrome_ll.java
+++ NeetCode/Arrays and Hashing/Valid_Palindrome_ll.java
@@ -1,25 +1,30 @@
 public class Valid_Palindrome_ll {
-    public static boolean CheckPalindrome(String s){
-        int n = s.length();
-        int left = 0, right = n-1;
-        while(left < right){
-            if(s.charAt(right) == s.charAt(left+1)){
-                left++;
-            }
-            if(s.charAt(left) == s.charAt(right - 1)){
-                right--;
-            }
-            if(s.charAt(left) != s.charAt(right)){
-                return false;
+
+    public static boolean CheckPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                // Try skipping either left or right character
+                return isPalindromeRange(s, left + 1, right) || isPalindromeRange(s, left, right - 1);
             }
             left++;
             right--;
         }
         return true;
     }
-    public static void main(String[] args) {
-        String s = "abxxza";
-        System.out.println(CheckPalindrome(s));
+
+    private static boolean isPalindromeRange(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left++) != s.charAt(right--)) {
+                return false;
+            }
+        }
+        return true;
     }
-    
+
+    public static void main(String[] args) {
+        String s = "acdccba";
+        System.out.println(CheckPalindrome(s)); // ✅ Output: true
+    }
 }
