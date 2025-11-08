47de76eb2fcab9ca139a6be3235cd04274166f23
bytecreater <nihalahemad2003@gmail.com>
2025-11-08 10:58:54 +0530

    Valid Parentheses

---
+++ NeetCode/LinkedList/Add_two_numbers.java
@@ -0,0 +1,6 @@
+public class Add_two_numbers {
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ NeetCode/Stack/Valid_Parentheses.java
@@ -0,0 +1,36 @@
+package Stack;
+import java.util.HashMap;
+import java.util.Stack;
+
+public class Valid_Parentheses {
+    public static boolean isValid(String s) {
+        HashMap<Character,Character> map = new HashMap<>();
+        Stack<Character> st = new Stack<>();
+        map.put('(', ')');
+        map.put('{', '}');
+        map.put('[', ']');
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c == '(' || c == '{' || c == '[' ){
+                st.push(c);
+            }
+            else if(map.containsValue(c)){
+                if(st.isEmpty()){
+                    return false;
+                }
+                if(c == map.get(st.peek())){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+    public static void main(String[] args) {
+        String s = "[{})]";
+        System.out.println(isValid(s));
+        
+    }
+}
